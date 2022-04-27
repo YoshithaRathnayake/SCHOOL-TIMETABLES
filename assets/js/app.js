008793b8c2834510206967ec0ab6aea987b904c3
YOSHITHA RATHNAYAKE <Y.S.B.Rathnayake2008@gmail.com>
2022-04-27 15:44:23 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,4 @@
+// User Name
+
 UserName = prompt("Hi, I am Yoshitha Rathnayake. What is your name?");
 alert("Hello, " + UserName + " Welcome to my School Timetables that I made using HTML Tables.");
